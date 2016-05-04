474b588e298010ee5f3fe3db3b376206442490de
Naomi Dushay <ndushay@stanford.edu>
2016-05-03 17:48:01 -0700

    first three passing tests

--- test/edu/stanford/dlss/wowza/SulWowzaTester.java
+++ test/edu/stanford/dlss/wowza/SulWowzaTester.java
@@ -1,54 +1,134 @@
 package edu.stanford.dlss.wowza;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+
+import org.apache.log4j.*;
+
+import com.wowza.wms.application.IApplicationInstance;
+import com.wowza.wms.application.WMSProperties;
+
+import java.io.ByteArrayOutputStream;
 
 public class SulWowzaTester {
 
 	@Test
-	public void testOnAppStart()
+	public void testOnAppStartBadUrl()
 	{
-		// test use of correct url
-		// test use of default url if none exists
+		// the only visible result of onAppStart is a log message
+		//  logger is the rootLogger, per test/resources/log4j.properties
+		Logger logger = Logger.getRootLogger();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		Layout layout = new SimpleLayout();
+		Appender appender = new WriterAppender(layout, out);
+		logger.addAppender(appender);
+
 		SulWowza testModule = new SulWowza();
-		
-		fail("Not yet implemented");
+
+		WMSProperties mockProperties = mock(WMSProperties.class);
+		when(mockProperties.getPropertyStr("stacksURL", testModule.defaultUrl)).thenReturn("badUrl");
+		IApplicationInstance appInstanceMock = mock(IApplicationInstance.class);
+		when(appInstanceMock.getProperties()).thenReturn(mockProperties);
+
+		try {
+			testModule.onAppStart(appInstanceMock);
+			String logMsg = out.toString();
+			assertThat(logMsg, allOf(containsString("Unable to initialize"), 
+									containsString("java.net.MalformedURLException"), 
+									containsString("no protocol")));
+		} finally {
+			logger.removeAppender(appender);
+		}
 	}
 
 	@Test
+	public void testOnAppStartDefaultUrl()
+	{
+		// the only visible result of onAppStart is a log message
+		//  logger is the rootLogger, per test/resources/log4j.properties
+		Logger logger = Logger.getRootLogger();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		Layout layout = new SimpleLayout();
+		Appender appender = new WriterAppender(layout, out);
+		logger.addAppender(appender);
+
+		SulWowza testModule = new SulWowza();
+
+		WMSProperties mockProperties = mock(WMSProperties.class);
+		when(mockProperties.getPropertyStr("stacksURL", testModule.defaultUrl)).thenReturn(testModule.defaultUrl);
+		IApplicationInstance appInstanceMock = mock(IApplicationInstance.class);
+		when(appInstanceMock.getProperties()).thenReturn(mockProperties);
+
+		try {
+			testModule.onAppStart(appInstanceMock);
+			String logMsg = out.toString();
+			assertThat(logMsg, containsString("Initialized " + testModule.getClass().getSimpleName() + " at " + testModule.defaultUrl));
+		} finally {
+			logger.removeAppender(appender);
+		}
+	}
+
+	@Test
+	public void testOnAppStartValidPropertyUrl()
+	{
+		// the only visible result of onAppStart is a log message
+		//  logger is the rootLogger, per test/resources/log4j.properties
+		Logger logger = Logger.getRootLogger();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		Layout layout = new SimpleLayout();
+		Appender appender = new WriterAppender(layout, out);
+		logger.addAppender(appender);
+
+		SulWowza testModule = new SulWowza();
+
+		WMSProperties mockProperties = mock(WMSProperties.class);
+		when(mockProperties.getPropertyStr("stacksURL", testModule.defaultUrl)).thenReturn("http://example.org");
+		IApplicationInstance appInstanceMock = mock(IApplicationInstance.class);
+		when(appInstanceMock.getProperties()).thenReturn(mockProperties);
+		
+		try {
+			testModule.onAppStart(appInstanceMock);
+			String logMsg = out.toString();
+			assertThat(logMsg, containsString("Initialized " + testModule.getClass().getSimpleName() + " at http://example.org"));
+		} finally {
+			logger.removeAppender(appender);
+		}
+	}
+
+//	@Test
 	public void testOnHTTPMPEGDashStreamingSessionCreate()
 	{
-		// always succeed at the moment
-		
-	}
-
-	@Test
-	public void testOnHTTPCupertinoStreamingSessionCreate()
-	{
-		// always succeed at the moment
 		fail("Not yet implemented");
 	}
-	
-	@Test
+
+//	@Test
+	public void testOnHTTPCupertinoStreamingSessionCreate()
+	{
+		fail("Not yet implemented");
+	}
+
+//	@Test
 	public void testAuthorizeSession()
 	{
 		fail("Not yet implemented");
 	}
-	
-	@Test
+
+//	@Test
 	public void testAuthorize()
 	{
 		fail("Not yet implemented");
 	}
-	
-	@Test
+
+//	@Test
 	public void testGetAuthToken()
 	{
 		fail("Not yet implemented");
 	}
-	
-	@Test
+
+//	@Test
 	public void testValidateAuthToken()
 	{
 		fail("Not yet implemented");
